72b3e34ed6a93048843b81f814d1cc9ff44d5ffe
Tatiana Rodriguez <tatiana.rodriguez@morningstar.com>
2025-02-23 16:10:15 -0500

    feat: add and configure custom fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,36 +1,43 @@
 import { Stack } from "expo-router";
 import { FavoritesProvider } from "../context/FavoriteProvider";
-import { useFonts } from 'expo-font';
+import { useFonts } from "expo-font";
 import { useEffect } from "react";
-import * as SplashScreen from 'expo-splash-screen';
+import * as SplashScreen from "expo-splash-screen";
 
 export const unstable_settings = {
-    initialRouteName: '(tabs)',
+  initialRouteName: "(tabs)",
 };
 
 SplashScreen.preventAutoHideAsync();
 
 export default function Layout() {
-    const [loaded] = useFonts({
-      SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    });
-  
-    useEffect(() => {
-      if (loaded) {
-        SplashScreen.hideAsync();
-      }
-    }, [loaded]);
-  
-    if (!loaded) {
-      return null;
+  const [loaded] = useFonts({
+    OswaldBold: require("../assets/fonts/Oswald-Bold.ttf"),
+    OswaldLight: require("../assets/fonts/Oswald-Light.ttf"),
+    OswaldMedium: require("../assets/fonts/Oswald-Medium.ttf"),
+    OswaldRegular: require("../assets/fonts/Oswald-Regular.ttf"),
+    OswaldSemiBold: require("../assets/fonts/Oswald-SemiBold.ttf")
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
     }
-  
-    return (
-        <FavoritesProvider>
-            <Stack>
-                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                <Stack.Screen name="planetDetails" options={{ title: 'Detalles del Planeta' }} />
-            </Stack>
-        </FavoritesProvider>
-    );
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <FavoritesProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen
+          name="planetDetails"
+          options={{ title: "Detalles del Planeta" }}
+        />
+      </Stack>
+    </FavoritesProvider>
+  );
 }
