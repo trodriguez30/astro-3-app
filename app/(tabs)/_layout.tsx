77e3be227c4743138bb7af0d53e05b5284359b22
Tatiana Rodriguez <tatiana.rodriguez@morningstar.com>
2025-02-23 16:11:26 -0500

    feat: customize tab bar appearance and behavior

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,50 @@
-import { Tabs } from 'expo-router';
-
+import { FontAwesome } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import { View } from "react-native";
 
 export default function TabsLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Planetas" }} />
-      <Tabs.Screen name="favorites" options={{ title: "Favoritos" }} />
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "#1E2156",
+          borderTopWidth: 1,
+          borderTopColor: "#2F365F", 
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View style={{ width: 40, height: 50,  justifyContent: "flex-end",
+              alignItems: "center", marginTop: 10}}>
+            <FontAwesome
+              name="home"
+              size={40}
+              color={focused ? "#FFD700" : "#7A84C1"}
+            />
+            </View>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="favorites"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View style={{ width: 40, height: 40,  justifyContent: "flex-end",
+              alignItems: "center", marginTop: 10}}>
+            <FontAwesome
+              name="heart"
+              size={35}
+              color={focused ? "#FFD700" : "#7A84C1"}
+            />
+            </View>
+          ),
+        }}
+      />
     </Tabs>
   );
 }
